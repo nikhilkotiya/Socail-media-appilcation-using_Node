d19f4720133bce7892ba38feec93ac9bf618fbfc
nikhilkotiya8 <nikhilkotiya8@gmail.com>
2022-05-11 22:44:03 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const mongoose=require("mongoose")
+const { stringify } = require("nodemon/lib/utils")
+
+const users=new mongoose.Schema({
+
+    username:{
+        type : String,
+        required:true,
+        min:5,
+        max:40,
+        unique:true
+    },
+    email:{
+      type:String,
+      required:true,
+      max:49,
+      unique:true  
+    },
+    password:{
+        type:String,
+        required:true,
+        min:6,
+    },
+    profile_pic:{
+        type:String,
+        default:"",
+    },
+    followers:{
+        type:Array,
+        default:[],
+    },
+    following:{
+        type:Array,
+        default:[],
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    },
+},
+{timestamps:true}
+);
+
+module.exports =  mongoose.model("User",users)
---
+++ routes/user.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+
+// router.get("/",(req,res)=>{
+//     res.send("hey user root")
+// })
+// Update use
+
+module.exports = router
